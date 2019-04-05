4a2da5c740f49a013ba762218a5d99f00850be36
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2019-04-04 17:13:53 -0700

    polished final 3rd section

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Header from './components/Header/Header';
 import FirstSection from './components/Body/FirstSection';
 import SecondSection from './components/Body/SecondSection';
 import ThirdSection from './components/Body/ThirdSection';
-import Carousel from './components/Body/Carousel';
+// import Carousel from './components/Body/Carousel';
 import reset from './constants/css/reset';
 
 const GlobalStyle = createGlobalStyle`${reset}`;
